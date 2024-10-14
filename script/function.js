41be04ecbbae0ea8d6b619ff09a1847b8b4edabb
ShiktoBakshi <147204180+ShiktoBakshi@users.noreply.github.com>
2024-10-14 12:16:30 +0600

    card design complete

--- script/function.js
+++ script/function.js
@@ -1,25 +1,80 @@
 
 
-const LoadCategories = ()=>{
+const loadCategories = ()=>{
     fetch("https://openapi.programming-hero.com/api/peddy/categories")
     .then((res)=>res.json())
     .then((data)=> displayCategories(data.categories))
     .catch((error)=>console.log(error));
 };
-const LoadCards = ()=>{
+const loadCards = ()=>{
     fetch("https://openapi.programming-hero.com/api/peddy/pets")
     .then((res)=>res.json())
     .then((data)=>displayPets(data.pets))
     .catch((error)=>console.log(error));
 };
+const loadCategoryCard=(id)=> {
+    
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`)
+    .then((res)=>res.json())
+    .then((data) =>{
+        removeActiveClass()
+         const activeBtn=document.getElementById(`btn-${id}`)
+         activeBtn.classList.add("active");
+        displayPets(data.data);
+    })
+   
+};
+const removeActiveClass=()=>{
+    const buttons= document.getElementsByClassName(`category-btn`);
+    for(let btn of buttons){
+     btn.classList.remove("active")
+    };
+ };
+ const loadDetails=async(cardsId)=>{
+    console.log(cardsId);
+    const uri=`https://openapi.programming-hero.com/api/peddy/category/${cardsId}`;
+    const res=await fetch(uri);
+    const data =await res.json();
+    displayDetails(data.data)
+ }
+ const displayDetails=(card)=>{
+    console.log(card);
+    const detailContainer=document.getElementById("modal-box");
+    
+    detailContainer.innerHTML=
+    `
+    <img src=${card.data}/>
+    `
+
+    document.getElementById("customModal").showModal();
+ }
 
 const displayPets =(pets) =>{
+   
     const cardContainer=document.getElementById("cards");
+    cardContainer.innerHTML="";
+   
+    if(pets.length==0){
+        cardContainer.classList.remove("grid");
+        cardContainer.innerHTML=`
+        <div class="h-[200px]  flex flex-col gap-5 justify-center items-center  ">
+          <img src="images/error.webp"/>
+         <span class="text-xl font-bold text-center">
+         No Data Available
+         </span>
+        </div>
+        `;
+        return;
+    }
+    else{
+        cardContainer.classList.add("grid");
+    }
     pets.forEach((pet)=>{
-        console.log(pet);
+console.log(pet)
         const breed =pet.breed||"Not Available "
         const birth =pet.date_of_birth||"Not Available "
         const gender=pet.gender||"Not Available "
+        const price=pet.price||"Not Available "
         const card=document.createElement("div");
         card.classList="card px-2 py-2 border"
         card.innerHTML=
@@ -52,14 +107,14 @@ const displayPets =(pets) =>{
     </div>
     <div class="flex">
     <div class="h-5 w-5">
-    <img src="https://img.icons8.com/?size=48&id=i2o6gXhX4Euy&format=png"/>
+    <img src="https://img.icons8.com/?size=48&id=85782&format=png"/>
     </div>
-    <p> Price: ${pet.price} $ </p>
+    <p> Price: ${price} </p>
     </div>
     <hr/>
     <div class="flex justify-center gap-5 py-5 ">
      <div class="  px-4  border border-7 border-black rounded-lg">
-     <button class="w-8 h-8 py-2">
+     <button   class="w-8 h-8 py-2">
      <img src="https://img.icons8.com/?size=48&id=82788&format=png">
      </button>
      </div>
@@ -68,7 +123,7 @@ const displayPets =(pets) =>{
  
      </div>
      <div>
-         <button class="btn btn-outline">Details</button>
+         <button onclick="loadDetails('${pet.category}')"  class="btn btn-outline">Details</button>
      </div>
      
    </div>
@@ -77,18 +132,25 @@ const displayPets =(pets) =>{
         cardContainer.append(card);
     })
 }
-const displayCategories = (categories)=>{
-    const categoryContainer=document.getElementById("categories");
-        
-        categories.forEach((item)=>{
-            const button = document.createElement("button");
-            button.classList="btn"
-            button.innerText = item.category;
 
-            categoryContainer.append(button);
+const displayCategories= (categories)=>{
+    const categoryContainer=document.getElementById("categories");
+   
+    categories.forEach((item) => {
+           const buttonContainer=document.createElement('div');
+           buttonContainer.innerHTML=
+           `
+           <button id="btn-${item.category}" onclick="loadCategoryCard('${item.category}')" class="btn category-btn   ">
+           <img class="h-6 w-6 lg:h-10 lg:w-10" src="${item.category_icon}"/>
+           ${item.category}
+           
+           </button>
+            `;
+
+            categoryContainer.append(buttonContainer);
 
         });
    
 };
-LoadCategories();
-LoadCards();
+loadCategories();
+loadCards();
